b2741cee79c8a739a8db9ca0519a84ac2b515b0c
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-29 21:04:43 +0300

    get role fuction

---
+++ routers/routes/role.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createRole, roles } = require("./../controllers/role");
+const roleRouter = express.Router();
+////get role function
+roleRouter.post("/createrole", createRole);
+roleRouter.get("/roles", roles);
+
+module.exports = roleRouter;
