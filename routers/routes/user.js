629c61eade79340540c9dc7c501abdf278505724
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-29 21:09:44 +0300

    add login/register function

---
+++ routers/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const  {register ,login} = require('./../controllers/user')
+
+const userRouter = express.Router()
+
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+
+
+///export 
+module.exports = userRouter;
